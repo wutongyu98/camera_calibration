fd73ced4243bfced1acc6156806af101a169fb9e
wutongyu98 <43251463+wutongyu98@users.noreply.github.com>
2020-07-01 16:56:31 -0400

    Add files via upload

---
+++ ImageSourceCameraCleanCapture/src/talker.cpp
@@ -0,0 +1,129 @@
+#include <opencv2/highgui/highgui.hpp>
+#include "opencv2/opencv.hpp"
+#include <string>
+#include <opencv2/core/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include "tcamimage.h"
+
+
+using namespace gsttcam;
+using namespace std;
+using namespace cv;
+int exposure_time = 5000, w = 1440, h = 1080;
+
+
+int main(int argc, char** argv)
+{
+    // Initialize our TcamCamera object "cam" with the serial number
+    // of the camera, which is to be used in this program.
+    // my camera is here 03020388 and 03020395
+    TcamImage cam("03020388"); 
+    TcamImage cam2("03020395"); 
+    
+    cam.set_capture_format("BGRx", FrameSize{1440,1080}, FrameRate{30,1});
+    cam2.set_capture_format("BGRx", FrameSize{1440,1080}, FrameRate{30,1});
+    
+    // Start the camera
+    cam.start();
+    cam2.start();
+    cam.set_auto_exposure();
+    cam.set_exposure_time(exposure_time);
+    cam2.set_auto_exposure();
+    cam2.set_exposure_time(exposure_time);
+    
+    std::shared_ptr<Property> GainValue = NULL;
+    
+    try
+    {
+        GainValue = cam.get_property("Gain");
+    }
+    catch(std::exception &ex)    
+    {
+        printf("Error %s : %s\n",ex.what(), "Gain Value");
+    }
+    
+    if( GainValue != NULL){
+        cout <<"I am setting gain for cam " << endl;
+        GainValue->set(cam,150);
+    }
+    
+    std::shared_ptr<Property> GainValue2 = NULL;
+    
+    
+    try
+    {
+        GainValue2 = cam2.get_property("Gain");
+    }
+    catch(std::exception &ex)    
+    {
+        printf("Error %s : %s\n",ex.what(), "Gain Value");
+    }
+    
+    if( GainValue2 != NULL){
+        cout <<"I am setting gain for cam2 " << endl;
+        GainValue2->set(cam2,150);
+    }
+    
+    
+    std::shared_ptr<Property> GainAuto = NULL;
+    
+    GainAuto = cam.get_property("Gain Auto");
+    if( GainAuto != NULL)
+    {
+        int Auto;
+        GainAuto->get(cam,Auto);
+        if( Auto == 1)
+            printf("Current gain automatic is On.\n");
+        else
+            printf("Current gain  automatic is Off.\n");
+    }
+    
+    
+    if( GainValue != NULL)
+    {
+        int gain;
+        GainValue->get(cam,gain);
+        printf("Current gain value is %d.\n",gain);
+    }
+    
+    if( GainValue2 != NULL)
+    {
+        int gain2;
+        GainValue2->get(cam2,gain2);
+        printf("Current gain2 value is %d.\n",gain2);
+    }
+    
+    sleep(1);
+    
+    
+    
+    Mat frame, frame2;    
+    
+    while (1) 
+    {
+        
+        cam.snapImage(60);
+        cam2.snapImage(60);
+        
+        frame.create( cam.getHeight(),cam.getWidth(),CV_8UC(cam.getBytesPerPixel()));
+        memcpy( frame.data, cam.getImageData(), cam.getImageDataSize());
+        
+        frame2.create( cam2.getHeight(),cam2.getWidth(),CV_8UC(4));
+        memcpy( frame2.data, cam2.getImageData(), cam2.getImageDataSize());
+        
+        
+        if(!frame.empty() & !frame2.empty())
+        {
+            imshow("left", frame);
+            imshow("right", frame2);
+            waitKey(1);
+        }
+        else
+            cout << "EMPTY FRAME!" << endl;
+        
+    }
+    return 0;
+}
---
+++ ImageSourceCameraCleanCapture/src/tcamimage.cpp
@@ -0,0 +1,99 @@
+#include "tcamimage.h"
+#include <unistd.h>
+#include <chrono>
+
+using namespace gsttcam;
+
+TcamImage::TcamImage(std::string serial) : TcamCamera(serial)
+{
+    _CustomData.ImageCounter = 0;
+    _CustomData.SaveNextImage = false;
+    _CustomData.bpp = 4;
+    _CustomData.width = 0;
+    _CustomData.height = 0;
+    _CustomData.image_data = NULL;
+}
+
+TcamImage::~TcamImage()
+{
+    if(_CustomData.image_data != NULL)
+    {
+        delete _CustomData.image_data;
+    }
+}
+
+void TcamImage::set_capture_format(std::string format, FrameSize size, FrameRate framerate)
+{
+    // Allocate memory for one image buffer.
+    if( format == "GRAY8")
+        _CustomData.bpp = 1;
+    else
+        _CustomData.bpp = 4;
+
+    _CustomData.width = size.width;
+    _CustomData.height = size.height;
+    //_CustomData.image_data.resize( size.width * size.width * _CustomData.bpp );
+
+    TcamCamera::set_capture_format(format, size, framerate);
+}
+
+bool TcamImage::start()
+{
+    // Register a callback to be called for each new frame
+    set_new_frame_callback(new_frame_cb, &_CustomData);
+    if(_CustomData.image_data != NULL)
+    {
+        delete _CustomData.image_data;
+    }
+    _CustomData.image_data = new unsigned char[getImageDataSize()] ;
+    TcamCamera::start();
+}
+
+bool TcamImage::snapImage(int timeout_ms)
+{
+    std::unique_lock<std::mutex> lck(_CustomData.mtx);
+    _CustomData.SaveNextImage = true;
+    if( _CustomData.con.wait_for(lck,std::chrono::milliseconds(timeout_ms) ) != std::cv_status::timeout ){
+        return true;
+    }
+    return false;
+}
+
+////////////////////////////////////////////////////////////////////
+// Callback called for new images by the internal appsink
+GstFlowReturn TcamImage::new_frame_cb(GstAppSink *appsink, gpointer data)
+{
+    // Cast gpointer to CUSTOMDATA*
+    CUSTOMDATA *pCustomData = (CUSTOMDATA*)data;
+    if( !pCustomData->SaveNextImage)
+        return GST_FLOW_OK;
+    std::lock_guard<std::mutex> lck(pCustomData->mtx);
+
+    pCustomData->SaveNextImage = false;
+    pCustomData->ImageCounter++;
+
+    // The following lines demonstrate, how to access the image
+    // data in the GstSample.
+    GstSample *sample = gst_app_sink_pull_sample(appsink);
+
+    GstBuffer *buffer = gst_sample_get_buffer(sample);
+
+    GstMapInfo info;
+
+    gst_buffer_map(buffer, &info, GST_MAP_READ);
+    
+    if (info.data != NULL) 
+    {
+        // info.data contains the image data as blob of unsigned char 
+        memcpy( pCustomData->image_data, info.data, pCustomData->width * pCustomData->height * pCustomData->bpp);
+        //memcpy( pCustomData->image_data.data(), info.data, pCustomData->width * pCustomData->height * pCustomData->bpp);
+    }
+    
+    // Calling Unref is important!
+    gst_buffer_unmap (buffer, &info);
+    gst_sample_unref(sample);
+
+    pCustomData->con.notify_all();
+    // Set our flag of new image to true, so our main thread knows about a new image.
+    return GST_FLOW_OK;
+}
